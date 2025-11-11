761c5a16539772f030f52c75076f7c951299a4a8
Lay7415 <ieye7531@gmail.com>
2025-11-11 10:52:06 +0600

    feat(AssignmentOperations): learn assignment operations in java

---
+++ AssignmentOperations.java
@@ -0,0 +1,18 @@
+public class AssignmentOperations {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+        a += 10;
+        System.out.println(a);
+        a -= 3;
+        System.out.println(a);
+        a *= 2;
+        System.out.println(a);
+        a /= 6;
+        System.out.println(a);
+        a <<= 4;
+        System.out.println(a);
+        a >>= 2;
+        System.out.println(a);
+    }
+}
