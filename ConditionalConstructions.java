137d32cd2a0a71ffec019f2e540e544491f454fe
Lay7415 <ieye7531@gmail.com>
2025-11-11 10:59:08 +0600

    feat(ConditionalConstructions): learn ternar operations

--- ConditionalConstructions.java
+++ ConditionalConstructions.java
@@ -2,10 +2,15 @@ public class ConditionalConstructions {
     public static void main(String[] args) {
         String name = "Red Raptor";
 
-        if(name.equals("Red Raptor")){
+        if (name.equals("Red Raptor")) {
             System.out.println("Red Raptor is win!");
-        }else {
+        } else {
             System.out.println("Red Raptor is lose!");
         }
+
+        int x = 3;
+        int y = 2;
+        int z = x < y ? x + y : x - y;
+        System.out.println(z);
     }
 }
