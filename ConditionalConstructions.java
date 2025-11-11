6d73867f74ac0384ea21fc8f044cd295076e53b0
Lay7415 <ieye7531@gmail.com>
2025-11-11 10:57:48 +0600

    feat(ConditionalConstructions): learn conditional constructions

---
+++ ConditionalConstructions.java
@@ -0,0 +1,11 @@
+public class ConditionalConstructions {
+    public static void main(String[] args) {
+        String name = "Red Raptor";
+
+        if(name.equals("Red Raptor")){
+            System.out.println("Red Raptor is win!");
+        }else {
+            System.out.println("Red Raptor is lose!");
+        }
+    }
+}
