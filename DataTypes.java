560d65f6c32905356b39e22e9d0d586ebbc65804
Lay7415 <ieye7531@gmail.com>
2025-11-11 10:01:18 +0600

    feat(DataTypes): learn datatypes of java

---
+++ DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        boolean isActive = false;
+        byte a = 1;
+        short b = 2;
+        int c = 4;
+        long d = 8;
+        long d2 = 888888888L;
+        double e = 8.8;
+        float f = 4.4f;
+        char j = 'B';
+
+        System.out.println(isActive);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(d2);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(j);
+    }
+}
