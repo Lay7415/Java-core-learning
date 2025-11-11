1da2a4e8008941b6cf7125aabfb51682c06a0e64
Lay7415 <ieye7531@gmail.com>
2025-11-11 10:54:59 +0600

    feat(ConversionsOfBasicDataTypes): learn Conversions of basic data types in java

---
+++ ConversionsOfBasicDataTypes.java
@@ -0,0 +1,7 @@
+public class ConversionsOfBasicDataTypes {
+    public static void main(String[] args) {
+        double a = 56.9898;
+        int b = (int) Math.round(a);
+        System.out.println(b);
+    }
+}
